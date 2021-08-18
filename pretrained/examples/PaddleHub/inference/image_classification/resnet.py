83420d51d32143ad8547ec56e01837a1bd1c6ce5
Kenneth Church <kwchurch@us.ibm.com>
2021-08-18 11:29:18 -0700

    fine tuning

--- pretrained/examples/PaddleHub/inference/image_classification/resnet.py
+++ pretrained/examples/PaddleHub/inference/image_classification/resnet.py
@@ -1,28 +1,37 @@
 import paddle,sys,argparse
 import paddlehub as hub
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--labels", help="filename containing labels [defaults to Imagenet2012 categories]", default=None)
-args = parser.parse_args()
-
-
 # example usage
 # find flower_photos -name '*.jpg' | python resnet.py
 # find flower_photos -name '*.jpg' | python resnet.py --labels flower_photos/label_list.txt
 
 # for more flower pictures, download: https://bj.bcebos.com/paddlehub-dataset/flower_photos.tar.gz
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-l", "--labels", help="filename containing labels [defaults to Imagenet2012 categories]", default=None)
+parser.add_argument("-c", "--checkpoint", help="checkpoint from fine-tuning", default=None)
+args = parser.parse_args()
+
+if not args.checkpoint is None:
+    assert not args.labels is None, '--labels are required if --checkpoint is provided'
+
 def dict2str(d):
     return '\t'.join([ str(key) + ':' + str(d[key]) for key in d.keys() ])
 
 def lines(fd):
     return fd.read().rstrip().split('\n')
 
-
 if args.labels is None:    
-    model = hub.Module(name='resnet50_vd_imagenet_ssld')
+    model = hub.Module(name="resnet50_vd_imagenet_ssld")
 else:
     with open(args.labels, 'r') as fd: 
-        model = hub.Module(name='resnet50_vd_imagenet_ssld', label_list=lines(fd))
+        if not args.checkpoint is None:
+            model = hub.Module(
+                name='resnet50_vd_imagenet_ssld',
+                label_list=lines(fd),
+                load_checkpoint=args.checkpoint)
+        else:
+            model = hub.Module(name="resnet50_vd_imagenet_ssld", label_list=lines(fd))
 
 input_file_names = lines(sys.stdin)
 results = model.predict(input_file_names)
